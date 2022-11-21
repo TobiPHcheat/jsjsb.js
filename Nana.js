3c1b4580d04e63fa9442ad84a3718511ca2b8d89
TOBI_phcheat <110269240+TobiPHcheat@users.noreply.github.com>
2022-11-21 17:14:18 +0800

    Update Nana.js

--- Nana.js
+++ Nana.js
@@ -1,43 +1,22 @@
 async function maubucin(){
-
 await swals.fire({
-
-  title: '<strong>Nasa Humss B ba to?</strong>',
-
+  title: '<strong>Mau script bucin yang lain?</strong>',
   html:
-
-    'click mo yong Link  ' +
-
-    '<a href="https://bit.ly/htmlbucin2022">https://m.me/jandy.valesiojr</a> ' +
-
-    'clik it 😉' + '<br><br>' + 'You can edit it to your heart's content using the Quick Editor application or the like, thank you.',
-
+    'Cek scriptnya di ' +
+    '<a href="https://bit.ly/htmlbucin2022">https://bit.ly/scripthtmlbucin</a> ' +
+    'ya!' + '<br><br>' + 'Bisa diedit sesuka hatimu menggunakan aplikasi Quick Editor ataupun sejenisnya.',
   focusConfirm: false,
-
 });
-
 window.location = "https://bit.ly/htmlbucin2022";
-
 } 
-
 async function maubucin2(){
-
 await swals.fire({
-
   title: '<strong>Mau mentahan script bucinnya?</strong>',
-
   html:
-
     'Ambil scriptnya di: ' +
-
     '<a href="https://sfile.mobi/1KUBvuLh0k27">Klik di Sini!</a> ' +
-
     'ya' + '<br><br>' + 'Bisa diedit sesukamu menggunakan aplikasi Quick Editor ataupun HTML Editor lainnya.',
-
   focusConfirm: false,
-
 });
-
 window.location = "https://sfile.mobi/1KUBvuLh0k27";
-
 }
